f248c2ad59d75329ba6bdf545c3bb8041c085e43
Nitiwat1 <nitiwat9989@gmail.com>
2022-08-31 11:22:06 +0700

    8/31/2022

--- App.js
+++ App.js
@@ -1,29 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Page1 from './Screens/Page1';
-import Page2 from './Screens/Page2';
-import Page3 from './Screens/Page3';
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {
+    createDrawerNavigator,
+    DrawerContentScrollView,
+    DrawerItemList,
+    DrawerItem,
+} from "@react-navigation/drawer";
 
 const Stack = createNativeStackNavigator();
+
+function FeedScreen() {
+    return (
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+            <Text>Feed Screen</Text>
+        </View>
+    );
+}
+
+function ArticleScreen() {
+    return (
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+            <Text>Article Screen</Text>
+        </View>
+    );
+}
+function CustomDrawerContent(props) {
+    return (
+        <DrawerContentScrollView {...props}>
+            <DrawerItemList {...props} />
+            <DrawerItem label='Help' onPress={() => alert('Kink to Help')} />
+        </DrawerContentScrollView>
+    );
+}
+
+function MyDrawer() {
+    return (
+        <Drawer.Navigator
+            useLegacyImplementation
+            drawerContent={(props) => <CustomDrawerContent {...props} />}
+            screenOptions = {{
+                drawerStyle:{
+                    backgroundColor:'pink',
+                    width:240
+                }
+            }}
+            >
+            <Drawer.Screen name="Feed" component={FeedScreen} />
+            <Drawer.Screen name="Article" component={ArticleScreen} />
+        </Drawer.Navigator>
+    )
+}
+const Drawer = createDrawerNavigator();
 
 export default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator
-                initialRouteName='StackFP'
-                screenOptions={{
-                    headerStyle: { backgroundColor: '#FE603E' },
-                    headerTintColor: '#fff',
-                    headerTitleStyle: { fontWeight: 'bold', fontSize: 25 }
-                }}
-            >
-                <Stack.Screen name='First Page' component={Page1} />
-                <Stack.Screen name='Second Page' component={Page2} />
-                <Stack.Screen name='Third Page' component={Page3} />
-            </Stack.Navigator>
+            <MyDrawer />
         </NavigationContainer>
     );
 }
-
